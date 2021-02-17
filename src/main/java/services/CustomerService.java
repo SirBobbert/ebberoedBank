8a7905d9233f9c3c0a1257efcacfbadd2d843543
Robert Pallesen <Robert.pallesen13@gmail.com>
2021-02-17 11:16:09 +0100

    Update CustomerService

--- src/main/java/services/CustomerService.java
+++ src/main/java/services/CustomerService.java
@@ -1,34 +1,36 @@
 package services;
 
+import model.Account;
+import model.Customer;
 import util.Connector;
 
 import java.sql.*;
 
-public class CustomerService implements ICustomerService{
+public class CustomerService implements ICustomerService {
 
     @Override
     public int withdrawFunds(int x) {
 
-
+        Customer customer = new Customer("bob", "københavn", new Account(100, null));
 
         try {
 
             Connection conn = Connector.connection();
-            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM transactions", Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM accounts WHERE customerID = ?", Statement.RETURN_GENERATED_KEYS);
+
+            stmt.setInt(1, 1);
 
             ResultSet rs = stmt.executeQuery();
 
             while (rs.next()) {
-                int id = rs.getInt("id");
-                int amount = rs.getInt("amount");
+                int amount = rs.getInt("balance");
 
-                System.out.println(id + " " + amount);
+                System.out.println(amount);
             }
 
         } catch (SQLException | ClassNotFoundException e) {
             System.err.println("Fejl: " + e.getMessage());
         }
-
 
 
         return 0;
